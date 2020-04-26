1d872a8f23171733388dcb2172923c4e07b51517
paksi.peter <Bmevikmernokinfo96>
2020-04-26 18:13:02 +0200

    add game converter to db layer

--- app/src/main/java/com/example/vgames/model/db/entity/RoomGame.kt
+++ app/src/main/java/com/example/vgames/model/db/entity/RoomGame.kt
@@ -2,7 +2,6 @@ package com.example.vgames.model.db.entity
 
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
-import java.time.LocalDate
 
 @Entity(tableName = "games")
 data class RoomGame (
@@ -12,6 +11,6 @@ data class RoomGame (
     val image: String,
     val deck: String,
     val description: String,
-    val originalReleaseDate: LocalDate,
-    val platforms: List<String>
+    val originalReleaseDate: String,
+    val platforms: String
 )
